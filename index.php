afd200bec9e64bbd72c5a1495a7f72d6d57cea47
Eliiisa <elisaklein66@gmail.com>
2019-01-22 09:29:11 +0100

    HTML : page accueil

--- index.php
+++ index.php
@@ -8,7 +8,23 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <h1>Accueil</h1>
+
+                <div class="card p-4">
+                    <a href="inscription.php" class='btn btn-lg btn-primary m-4'>Inscription</a>
+                    <a href="connexion.php" class='btn btn-lg btn-warning m-4'>Connexion</a>
+                </div>
+               
+            </div>
+        </div>
+    </div>
     
+
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
